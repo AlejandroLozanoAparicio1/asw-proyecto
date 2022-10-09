db9c6f7a8de21962b703b773ac64f15144636597
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-10-09 22:33:52 +0200

    Fixed getUser

--- asw-back/src/main/java/com/example/demo/User/UserService.java
+++ asw-back/src/main/java/com/example/demo/User/UserService.java
@@ -1,11 +1,18 @@
-package com.example.demo.User;
-
+package com.example.demo;
+import com.example.demo.User.HackNewsRepository;
+import com.example.demo.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Service
 public class UserService {
+
+    UserService(){
+
+    }
     @Autowired
     private HackNewsRepository hackNewsRepository;
     public List<User> getUser(){
