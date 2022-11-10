954de1f44235b45342ce28c457446916c531bddd
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-11-10 15:10:36 +0100

    Commentaries working.

--- asw-back/src/main/resources/Static/js/profile.js
+++ asw-back/src/main/resources/Static/js/profile.js
@@ -1,6 +1,6 @@
 
 async function getUserVariables(username){
-    const response = await fetch("http://localhost:8081/user?username="+"oriol");
+    const response = await fetch("http://localhost:8081/user?username="+username);
     return await response.json();
 }
 function dataCorrectForm(maxvisit, minaway, delay){
@@ -33,36 +33,8 @@ async function setUserInfo() {
     document.getElementById("about").value = data.about
     if(data.showdead == 0) document.getElementById("showdead").checked = true
     if(data.noprocrast == 0) document.getElementById("noprocrast").checked = true
-
 }
 
 window.onload = async function WindowLoad(event) {
     setUserInfo()
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-    console.log('User signed out.');
-    });
-}
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-}
-window.handleCredentialResponse = async (response) => {
-    responsePayload = parseJwt(response.credential);
-    await fetch("http://localhost:8081/login?username=" + responsePayload.given_name, {method:'POST'});
-    localStorage.setItem("username", "username")
-    console.log(response.credential)
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
 }
