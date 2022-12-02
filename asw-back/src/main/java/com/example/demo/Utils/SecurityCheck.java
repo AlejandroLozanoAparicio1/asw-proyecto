da472a03c878c0b48cf242288df174e1ad1122f4
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-12-02 01:42:03 +0100

    Fixing security

--- asw-back/src/main/java/com/example/demo/Utils/SecurityCheck.java
+++ asw-back/src/main/java/com/example/demo/Utils/SecurityCheck.java
@@ -2,21 +2,25 @@ package com.example.demo.Utils;
 
 import com.example.demo.User.User;
 import com.example.demo.User.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityCheck {
     private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public SecurityCheck(UserService userService) {
+    public SecurityCheck(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public boolean checkUserIsAuthenticated(String username, String apiKey){
-        User user = userService.checkByUsernameAndKey(username,apiKey);
+        User user = userService.getUserByKey(apiKey);
         try{
-            user.getUsername();
-            return true;
+            if(user.getUsername().equals(username))
+                return true;
+            return false;
         }
         catch (Exception e){
             return false;
